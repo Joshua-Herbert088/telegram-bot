778e0fdf1788dfe1e0e78d10c4e32200c63802b2
Joshua <joshua.herbert@student.kuleuven.be>
2024-11-19 17:07:16 +0100

    add the game

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const bot = new Telegraf(process.env.BOT_TOKEN || "");
 bot.on("message", async (ctx) => {
   if (ctx.message.from.username === "VISCHCKQTOR") {
     await ctx.reply("@VISCHCKQTOR the game");
+  } else {
+    if (ctx.message.text.contains("the game")) {
+      await ctx.reply("@VISCHCKQTOR has lost the game");
+    }
   }
 });
 
