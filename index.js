cda53e0bd5c9b5f3222c4277c6b29dc378318479
Joshua <joshua.herbert@student.kuleuven.be>
2024-11-20 15:47:29 +0100

    remove exports

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const { exec } = require("child_process");
 const { saveData } = require("./util");
 const fs = require("fs");
 
-export function sleep(ms) {
+function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export function loadData(filePath) {
+function loadData(filePath) {
   if (fs.existsSync(filePath)) {
     return JSON.parse(fs.readFileSync(filePath, "utf8"));
   }
 }
 
-export function saveData(filePath, data) {
+function saveData(filePath, data) {
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 }
 
